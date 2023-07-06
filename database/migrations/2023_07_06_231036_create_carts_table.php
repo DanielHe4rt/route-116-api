6d8c6e787befd32d74cdc07a15d8b16bf4bc7b37
danielhe4rt <hey@danielheart.dev>
2023-07-06 20:28:12 -0300

    feat: migrations

---
+++ database/migrations/2023_07_06_231036_create_carts_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('customer_id')->constrained('customers');
+            $table->tinyInteger('status_id');
+            $table->tinyInteger('table_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('carts');
+    }
+};
