1861cfbc7a4223243aa080e5c6a6737978b37b97
danielhe4rt <hey@danielheart.dev>
2023-07-17 11:39:26 -0300

    wip

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/customers');
+Route::post('/carts');
+Route::post('/carts/{cartId}/products/{productId}');
+
+Route::get('/checkout/{cartId}');
+Route::post('/checkout/{cartId}'); // enviar descontos, metodo etc
+
